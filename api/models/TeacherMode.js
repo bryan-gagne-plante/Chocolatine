08aed967237c010827126543064962c991bb1093
Thower56 <Charles650@msn.com>
2024-04-09 20:15:58 -0400

    Correction de la marde de comflit que jai fait ahah

--- api/models/TeacherMode.js
+++ api/models/TeacherMode.js
@@ -1,34 +1,3 @@
-<<<<<<< Updated upstream
-const teacher = {
-  teacherMode: process.env.TEACHERMODE,
-  teacherPromptEnable: process.env.TEACHER_PROMPT_ENABLED,
-  teacherPrompt: process.env.TEACHER_PROMPT,
-  teacherSubjects: process.env.TEACHER_SUBJECTS,
-};
-
-function isTeacherMode() {
-  return teacher.teacherMode;
-}
-
-function isTeacherPromptEnable() {
-  return teacher.teacherPromptEnable;
-}
-
-async function addTeacherPrompt(prompt) {
-  return prompt + teacher.teacherPrompt;
-}
-
-async function teacherSubjects() {
-  return teacher.teacherSubjects;
-}
-
-module.exports = {
-  isTeacherMode,
-  isTeacherPromptEnable,
-  addTeacherPrompt,
-  teacherSubjects,
-};
-=======
 const teacher = {
   teacherMode: process.env.TEACHERMODE,
   teacherPromptEnable: process.env.TEACHER_PROMPT_ENABLED,
@@ -52,4 +21,3 @@ module.exports = {
   isTeacherPromptEnable,
   addTeacherPrompt,
 };
->>>>>>> Stashed changes
--- api/server/routes/index.js
+++ api/server/routes/index.js
@@ -1,4 +1,3 @@
-<<<<<<< Updated upstream
 const ask = require('./ask');
 const edit = require('./edit');
 const messages = require('./messages');
@@ -20,6 +19,7 @@ const assistants = require('./assistants');
 const files = require('./files');
 const isTeacher = require('./isTeacher');
 const teacherSubjects = require('./teacherSubjects');
+const role = require('./role');
 
 module.exports = {
   search,
@@ -43,49 +43,5 @@ module.exports = {
   files,
   isTeacher,
   teacherSubjects,
-};
-=======
-const ask = require('./ask');
-const edit = require('./edit');
-const messages = require('./messages');
-const convos = require('./convos');
-const presets = require('./presets');
-const prompts = require('./prompts');
-const search = require('./search');
-const tokenizer = require('./tokenizer');
-const auth = require('./auth');
-const keys = require('./keys');
-const oauth = require('./oauth');
-const endpoints = require('./endpoints');
-const balance = require('./balance');
-const models = require('./models');
-const plugins = require('./plugins');
-const user = require('./user');
-const config = require('./config');
-const assistants = require('./assistants');
-const files = require('./files');
-const role = require('./role');
-
-module.exports = {
-  search,
-  ask,
-  edit,
-  messages,
-  convos,
-  presets,
-  prompts,
-  auth,
-  keys,
-  oauth,
-  user,
-  tokenizer,
-  endpoints,
-  balance,
-  models,
-  plugins,
-  config,
-  assistants,
-  files,
   role,
 };
->>>>>>> Stashed changes
