eab3632f26167370091ef5c248c58c558540e01e
laurentfaggin <laurentfaggin@hotmail.fr>
2024-04-11 08:33:22 -0400

    suppression import Teacher dans index de ui

--- client/src/components/ui/index.ts
+++ client/src/components/ui/index.ts
@@ -20,7 +20,6 @@ export * from './Templates';
 export * from './Textarea';
 export * from './TextareaAutosize';
 export * from './Tooltip';
-export * from '../Chat/Menus/Subject/SubjectTeacher';
 export { default as Dropdown } from './Dropdown';
 export { default as FileUpload } from './FileUpload';
 export { default as DelayedRender } from './DelayedRender';
