02902a2121402552e2e8f5e9e26690d0ce9d0ae3
laurentfaggin <laurentfaggin@hotmail.fr>
2024-04-11 09:30:29 -0400

    suppression des console.log

--- api/server/routes/isTeacher.js
+++ api/server/routes/isTeacher.js
@@ -5,7 +5,6 @@ const router = express.Router();
 
 router.get('/', async (req, res) => {
   res.status(200).json(await isTeacher.isTeacherMode());
-  console.log('isTeacherMode:', await isTeacher.isTeacherMode());
 });
 
 module.exports = router;
--- api/server/routes/teacherSubjects.js
+++ api/server/routes/teacherSubjects.js
@@ -5,7 +5,6 @@ const router = express.Router();
 
 router.get('/', async (req, res) => {
   res.status(200).json(await teacherSubjects.teacherSubjects());
-  console.log('teacherSubjects:', await teacherSubjects.teacherSubjects());
 });
 
 module.exports = router;
