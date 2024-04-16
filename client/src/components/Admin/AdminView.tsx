02be7a39e3fb0904065463fb237298621fb9373a
Thower56 <Charles650@msn.com>
2024-04-16 17:52:20 -0400

    Ajout d'un container dans Admin

--- client/src/components/Admin/AdminView.tsx
+++ client/src/components/Admin/AdminView.tsx
@@ -12,16 +12,18 @@ const AdminView = () => {
   };
 
   return (
-    <>
-      <h1>Admin View</h1>
-      <div>
-        <Button className="bg-indigo-500 hover:bg-indigo-600 text-white px-4 py-2 rounded" onClick={returnToChat}>
-          <span>Retourne au Chat</span>
-        </Button>
+    <div className='flex items-center justify-center min-h-screen'>
+      <div className='container'>
+        <h1 className='text-white px-4 py-2 rounded'>Admin View</h1>
+        <div>
+          <Button className="bg-indigo-500 hover:bg-indigo-600 text-white px-4 py-2 rounded" onClick={returnToChat}>
+            <span>Retourne au Chat</span>
+          </Button>
+        </div>
+        <TokenUsage/>
+        <ThemeSelector />
       </div>
-      <TokenUsage/>
-      <ThemeSelector />
-    </>
+    </div>
   );
 };
 
